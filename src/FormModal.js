89d4e35dc9ab97236c2a07005e41b8d7835fe657
野上信悟 <nogamishingo@MacBook-Air-for-Shingo-Nogami.local>
2024-11-28 10:30:19 +0900

    11. Contextを利用したテーマの変更

--- src/FormModal.js
+++ src/FormModal.js
@@ -1,12 +1,16 @@
 import { Modal } from './components/modal';
 import { Button } from './components/button';
 import styled from 'styled-components';
+import { THEMES, ThemeContext } from './contexts/ThemeContext';
+import { useContext } from 'react';
 
 const Container = styled.div`
     width: 240px;
     border-radius: 10px;
     padding: 24px 36px;
-    background-color: white;
+    color: ${({theme})=>theme.color};
+    background-color: ${({theme})=>theme.backgroundColor};
+    border: ${({theme})=>(theme===THEMES.dark) ? '2px solid white' : 'none'};
 `
 
 const ButtonWrapper = styled.div`
@@ -16,9 +20,10 @@ const ButtonWrapper = styled.div`
 `
 
 export const FormModal = ({ confirm, cancel }) => {
+    const [theme, toggleTheme] = useContext(ThemeContext);
     return (
         <Modal>
-            <Container>
+            <Container theme={theme}>
                 <div>本当に作成しますか？</div>
                 <ButtonWrapper>
                     <Button onClick={confirm}>OK</Button>
